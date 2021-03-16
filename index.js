2fbc9ce16db6765ad0a3f4f0ad4621132c34b5de
github username <github email address>
2021-03-16 13:34:35 -0700

    commit tree 8c02ba2e7619e5928924ee2af121dbbb77408f1a

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+let string
+
+function writeCards(str,event){
+    let arr = []
+
+    for (let i = 0; i < str.length; i++) {
+        arr.push(`Thank you, ${str[i]}, for the wonderful ${event} gift!`)
+    }
+
+    return arr
+}
+
+function countDown(num){
+    let i = num;
+
+    while (i >= 0){
+        console.log(i--)
+    }
+}
